6986535559f6be09826a819d4111eadeb5883560
Тимур Багаутдинов <timur@iMac-Timur.local>
2018-09-10 21:19:31 +0300

    FT uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,33 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """
+    Test for a new visitor
+    """
 
-assert 'Djangor' in browser.title
+    def setUp(self):
+        """
+        setup
+        """
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """
+        tear down
+        """
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        test: become a list and get its later
+        """
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End Test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
