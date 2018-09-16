cc09a9bc2269ce0b7f6a2ee98d24d0546720e59d
Тимур Багаутдинов <timur@iMac-Timur.local>
2018-09-16 21:13:35 +0300

    HTML of home page is generated from the template

--- functional_tests.py
+++ functional_tests.py
@@ -45,7 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New item don't display in the table"
         )
 
         self.fail('End Test!')
